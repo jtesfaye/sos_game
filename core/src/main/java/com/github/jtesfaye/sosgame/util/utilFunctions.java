79f64c65daf9e36bd8effc55eea716e15a2e324a
jeremiah tesfaye <jt@jeremiahs-air.kc.umkc.edu>
2025-10-02 11:36:20 -0500

    add helper functions

--- core/src/main/java/com/github/jtesfaye/sosgame/util/utilFunctions.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/util/utilFunctions.java
@@ -1,4 +1,16 @@
 package com.github.jtesfaye.sosgame.util;
 
 public class utilFunctions {
+
+    public static int[] getBoardDimensions(String dim) {
+
+        String[] vals = dim.split("x");
+
+        int width = Integer.parseInt(vals[0]);
+        int height = Integer.parseInt(vals[1]);
+
+        return new int[] {width, height};
+
+    }
+
 }
