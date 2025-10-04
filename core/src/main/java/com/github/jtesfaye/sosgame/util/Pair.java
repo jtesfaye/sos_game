b8a7a88bab689d0ccb3ab9a1c1f245d3d7b77ea0
jeremiah tesfaye <jeremiahte@gmail.com>
2025-10-04 12:05:22 -0500

    add some util

--- core/src/main/java/com/github/jtesfaye/sosgame/util/Pair.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/util/Pair.java
@@ -1,4 +1,11 @@
 package com.github.jtesfaye.sosgame.util;
 
-public class Pair {
+public class Pair<T1, T2> {
+    public final T1 first;
+    public final T2 second;
+
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
 }
--- core/src/main/java/com/github/jtesfaye/sosgame/util/utilFunctions.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/util/utilFunctions.java
@@ -2,15 +2,13 @@ package com.github.jtesfaye.sosgame.util;
 
 public class utilFunctions {
 
-    public static int[] getBoardDimensions(String dim) {
+    public static Pair<Integer, Integer> getBoardDimensions(String dim) {
 
         String[] vals = dim.split("x");
 
         int width = Integer.parseInt(vals[0]);
         int height = Integer.parseInt(vals[1]);
 
-        return new int[] {width, height};
-
+        return new Pair<> (width, height);
     }
-
 }
