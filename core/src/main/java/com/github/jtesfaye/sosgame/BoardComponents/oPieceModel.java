874c6461cd5ed19ec6ffbfb5a5b4ac3cc6b809c8
jeremiah tesfaye <jeremiahte@gmail.com>
2025-10-27 13:38:08 -0500

    Add ability to change the color of a piece

--- core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/oPieceModel.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/oPieceModel.java
@@ -2,7 +2,10 @@ package com.github.jtesfaye.sosgame.BoardComponents;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 
 
@@ -10,10 +13,13 @@ public class oPieceModel {
 
     private final Model oPieceModel;
 
-    public oPieceModel() {
+    public oPieceModel(Color c) {
 
         ObjLoader loader = new ObjLoader();
         oPieceModel = loader.loadModel(Gdx.files.internal("oPiece.obj"));
+        for (Material m : oPieceModel.materials) {
+            m.set(ColorAttribute.createDiffuse(c));
+        }
 
     }
 
--- core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/sPieceModel.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/sPieceModel.java
@@ -1,17 +1,23 @@
 package com.github.jtesfaye.sosgame.BoardComponents;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 
 public class sPieceModel {
 
     private final Model sPieceModel;
 
-    public sPieceModel() {
+    public sPieceModel(Color c) {
 
         ObjLoader loader = new ObjLoader();
         sPieceModel = loader.loadModel(Gdx.files.internal("sPiece.obj"));
+        for (Material m : sPieceModel.materials) {
+            m.set(ColorAttribute.createDiffuse(c));
+        }
 
     }
 
