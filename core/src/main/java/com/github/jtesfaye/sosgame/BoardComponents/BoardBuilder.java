dfef18fe71a96a1bca71fe75053c4bd5c388802e
jeremiah tesfaye <jeremiahte@gmail.com>
2025-11-11 10:42:33 -0600

    changes

--- core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/BoardBuilder.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/BoardComponents/BoardBuilder.java
@@ -3,8 +3,8 @@ package com.github.jtesfaye.sosgame.BoardComponents;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.math.collision.BoundingBox;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.github.jtesfaye.sosgame.GameObject.Tile;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -71,5 +71,4 @@ public class BoardBuilder {
         }
         return tiles;
     }
-
 }
