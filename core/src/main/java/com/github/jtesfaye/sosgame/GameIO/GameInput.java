756502a7dd2a22d99f43c7fd44c4ac27a518ca0e
jeremiah tesfaye <jeremiahte@gmail.com>
2025-11-11 10:43:26 -0600

    change GameInput to work with ClickInputHandler

--- core/src/main/java/com/github/jtesfaye/sosgame/GameIO/GameInput.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/GameIO/GameInput.java
@@ -8,31 +8,43 @@ import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.collision.BoundingBox;
 import com.github.jtesfaye.sosgame.GameLogic.GameLogic;
-import com.github.jtesfaye.sosgame.GameLogic.Piece;
+import com.github.jtesfaye.sosgame.GameObject.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class GameInput extends InputAdapter {
 
     private final PerspectiveCamera camera;
     private final ArrayList<ArrayList<ModelInstance>> tiles;
 
+    private Map<UUID, ClickInputHandler> handlers;
     private final int row;
     private final int height;
+    private Player currentPlayer;
+    private final GameLogic logic;
 
-    private final InputHandler handler;
+    public GameInput(
+        PerspectiveCamera c,
+        ArrayList<ArrayList<ModelInstance>> t,
+        ArrayList<ClickInputHandler> handlers,
+        GameLogic logic) {
 
-
-    public GameInput(PerspectiveCamera c, ArrayList<ArrayList<ModelInstance>> t, InputHandler handler) {
         super();
-
         camera = c;
         tiles = t;
-
+        this.logic = logic;
         row = t.size();
         height = t.get(0).size();
+        this.handlers = new HashMap<>();
 
-        this.handler = handler;
+        for (ClickInputHandler h : handlers) {
+            this.handlers.put(h.playerId, h);
+        }
+
+        currentPlayer = logic.getCurrentTurn();
     }
 
     @Override
@@ -50,7 +62,9 @@ public class GameInput extends InputAdapter {
                 bounds.mul(tile.transform);
 
                 if (Intersector.intersectRayBoundsFast(ray, bounds)) {
-                    return handler.handleClick(r, c, button == Input.Buttons.LEFT);
+                    handlers.get(currentPlayer.getPlayerId()).handleClick(r, c, button == Input.Buttons.LEFT);
+                    currentPlayer = logic.getCurrentTurn();
+                    return true;
                 }
             }
         }
