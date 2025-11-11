6dbb0cf3e8db9644185b088f85d3a52df98dec74
jeremiah tesfaye <jeremiahte@gmail.com>
2025-11-11 11:39:52 -0600

    Add Computerinputhandler

--- core/src/main/java/com/github/jtesfaye/sosgame/GameIO/ComputerInputHandler.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/GameIO/ComputerInputHandler.java
@@ -1,4 +1,25 @@
 package com.github.jtesfaye.sosgame.GameIO;
 
-public class ComputerInputHandler {
+import com.github.jtesfaye.sosgame.GameObject.Move;
+import com.github.jtesfaye.sosgame.GameObject.Piece;
+import com.github.jtesfaye.sosgame.gameStrategy.GameStrategy;
+
+import java.util.UUID;
+
+public class ComputerInputHandler extends InputHandler {
+
+    GameStrategy strategy;
+
+    public ComputerInputHandler(UUID playerId, GameStrategy strat) {
+
+        super(playerId, InputType.Procedure);
+        strategy = strat;
+    }
+
+    @Override
+    public void getInput(Piece[][] board) {
+
+        Move move = strategy.makeMove(board);
+        consumer.accept(new InputEvent(move, playerId));
+    }
 }
