ca39076cd78ee36b2e0c760ac2906ed2b0ec4afe
jeremiah tesfaye <jeremiahte@gmail.com>
2025-11-10 20:15:31 -0600

    add playerFactory

--- core/src/main/java/com/github/jtesfaye/sosgame/util/PlayerFactory.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/util/PlayerFactory.java
@@ -1,4 +1,22 @@
 package com.github.jtesfaye.sosgame.util;
 
+import com.badlogic.gdx.graphics.Color;
+import com.github.jtesfaye.sosgame.GameObject.Player;
+
 public class PlayerFactory {
+
+    public static Player createPlayer(String type, Color color) {
+
+        switch (type) {
+            case "Human":
+                return new Player(Player.Type.Human, color);
+            case "Computer":
+                return new Player(Player.Type.Computer, color);
+            case "LLM":
+                return new Player(Player.Type.LLM, color);
+            default:
+                throw new RuntimeException("Invalid player type given");
+        }
+    }
+
 }
