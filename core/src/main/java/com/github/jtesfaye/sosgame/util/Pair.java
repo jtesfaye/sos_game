f15c6baf8e57b9daeab959172de6a29d0aaf8902
jeremiah tesfaye <jeremiahte@gmail.com>
2025-12-07 20:01:00 -0600

    add toString

--- core/src/main/java/com/github/jtesfaye/sosgame/util/Pair.java
+++ core/src/main/java/com/github/jtesfaye/sosgame/util/Pair.java
@@ -4,17 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
-public class Pair<T1, T2> {
-
-    @Getter
-    public final T1 first;
-
-    @Getter
-    public final T2 second;
+public record Pair<T1, T2>(@Getter T1 first, @Getter T2 second) {
 
     @JsonCreator
     public Pair(@JsonProperty("first") T1 first, @JsonProperty("second") T2 second) {
         this.first = first;
         this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s, %s)", first, second);
     }
 }
